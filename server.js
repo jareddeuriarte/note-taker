78a74e359c1da96d8f7bf8e80b0988f05badf92d
Jared de Uriarte <jared.deuri@gmail.com>
2021-04-05 18:19:23 -0400

    reading and writing to notes api. getting api on side of page

--- server.js
+++ server.js
@@ -1,45 +1,65 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 8080
 const path = require('path')
 const db = require('./db/db.json')
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-
-
-
-
-//GET Request - landing page and note entry path
+//GET Request - landing page and note entry path - HTML ROUTES 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/api/notes', (req, res) => {
+  console.log("Api route");
+  fs.readFile("./db/db.json", "utf8", (err, response) => {
 
+    if (err) throw err;
+
+    let existingNotes = JSON.parse(response);
+    console.log(response); 
+    res.json(existingNotes)
+  });
+})
+//HTML REQUEST FOR THE GET 
+app.get('/notes/', (req, res) => {
+  
+    console.log("HTML route");
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  
+});
+
+//API POST REQUEST 
 app.post('/notes', (req, res) => {
-    const newNote = req.body;
+  let newNote = req.body;
+  console.log("New note", newNote);
+  newNote = {...req.body, "id": uuidv4()};
+  fs.readFile("./db/db.json", "utf8", (err, response) => {
 
-    newNote.routeName = newNote.text.replace(/\s+/g, '').toLowerCase();
-    console.log(newNote);
+    if (err) throw err;
 
-    db.push(newNote);
-    res.json(newNote);
+    let existingNotes = JSON.parse(response);
+    console.log(response); 
 
-    let data = 'newNote'
+    //apend newNote to exisiting data 
+    existingNotes = [...existingNotes, newNote];
+    console.log("combining two data", existingNotes)
 
-    fs.writeFile('db/db.json', data, function (err) {
-        if (err) return console.log(err);
-        console.log(data)
-      });
+    fs.writeFile('db/db.json', JSON.stringify(existingNotes), function (err) {
+      if (err) return console.log(err);
+      // res.json(newNote);
+      console.log("Note created!", newNote);
+    });
 
+  }); //Closing Read  File 
 })
 
 
 
-
-
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
-  })
+  console.log(`App listening at http://localhost:${PORT}`)
+})
