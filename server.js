cb22f4a515136a40663712d88906cb22c6add24f
Jared de Uriarte <jared.deuri@gmail.com>
2021-04-05 10:04:07 -0400

    notes logging to console. fs to db needs work

--- server.js
+++ server.js
@@ -1,41 +1,45 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const path = require('path')
+const db = require('./db/db.json')
+const fs = require('fs')
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+
+
+
+//GET Request - landing page and note entry path
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.post('/notes', (req, res) => {
+    const newNote = req.body;
+
+    newNote.routeName = newNote.text.replace(/\s+/g, '').toLowerCase();
+    console.log(newNote);
+
+    db.push(newNote);
+    res.json(newNote);
+
+    let data = 'newNote'
+
+    fs.writeFile('db/db.json', data, function (err) {
+        if (err) return console.log(err);
+        console.log(data)
+      });
+
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
   })
-
-//GET Request - landing page
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
-
-// //POST Request
-// app.post('/', function (req, res) {
-//     res.send('Got a POST request')
-//   })
-
-// //PUT Request
-// app.put('/user', function (req, res) {
-//     res.send('Got a PUT request at /user')
-//   })
-
-
-
-
-
-// Acceptance Criteria
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
